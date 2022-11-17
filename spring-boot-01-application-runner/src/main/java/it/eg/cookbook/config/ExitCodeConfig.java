24164029130166466195a42c622cc6bd67cece47
Enrico Grillini <enrico.grillini@gmail.com>
2022-11-17 20:14:33 +0100

    refactor

--- spring-boot-01-application-runner/src/main/java/it/eg/cookbook/config/ExitCodeConfig.java
+++ spring-boot-01-application-runner/src/main/java/it/eg/cookbook/config/ExitCodeConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CustomConfig {
+public class ExitCodeConfig {
     @Bean
     public ExitCodeExceptionMapper exceptionBasedExitCode() {
         return exception -> 99;
