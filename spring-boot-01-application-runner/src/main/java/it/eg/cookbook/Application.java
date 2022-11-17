2ae0fab0448b860eaa7ab6aa34371bda3047f0ee
Enrico Grillini <enrico.grillini@gmail.com>
2022-11-17 07:55:14 +0100

    Prima semplice esecuzione

--- spring-boot-01-application-runner/src/main/java/it/eg/cookbook/Application.java
+++ spring-boot-01-application-runner/src/main/java/it/eg/cookbook/Application.java
@@ -1,11 +1,28 @@
 package it.eg.cookbook;
 
+import it.eg.cookbook.service.BatchService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-    public static void main (String[] args) {
-        SpringApplication.run(Application.class,args);
+@Slf4j
+public class Application implements ApplicationRunner {
+
+    @Autowired
+    BatchService batchService;
+
+    public static void main(String[] args) {
+        log.info("STARTING : Spring boot application starting");
+        SpringApplication.run(Application.class, args);
+        log.info("STOPPED  : Spring boot application stopped");
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        batchService.printParameters();
     }
 }
