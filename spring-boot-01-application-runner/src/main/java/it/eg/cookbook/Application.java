d78f18f9f565927fc2fac5ddea3ade6c4162fd01
Enrico Grillini <enrico.grillini@gmail.com>
2022-11-17 19:08:10 +0100

    test

--- spring-boot-01-application-runner/src/main/java/it/eg/cookbook/Application.java
+++ spring-boot-01-application-runner/src/main/java/it/eg/cookbook/Application.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ExitCodeExceptionMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
@@ -14,6 +16,20 @@ public class Application implements ApplicationRunner {
 
     @Autowired
     BatchService batchService;
+
+    @Bean
+    public ExitCodeExceptionMapper exceptionBasedExitCode() {
+        return exception -> {
+            if (exception.getCause() instanceof NumberFormatException) {
+                return 30;
+            }
+
+            if (exception.getCause() instanceof IllegalArgumentException) {
+                return 20;
+            }
+            return 99;
+        };
+    }
 
     public static void main(String[] args) {
         log.info("STARTING : Spring boot application starting");
@@ -24,5 +40,6 @@ public class Application implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
         batchService.printParameters();
+        throw new RuntimeException("sfiga");
     }
 }
